3aa61b84f15f16d3e7dfd39ec0fd825cc2f0bcc1
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-02 03:21:28 -0300

    Add try...catch no model

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,15 +1,25 @@
 const connection = require('./connection');
 
+const SERVER_ERROR = { status: 500, message: 'Internal Server Error' };
+
 const searchProductByName = async (name) => {
   const SELECT_PRODUCT = 'SELECT name FROM StoreManager.products WHERE name LIKE ?';
-  const [rows] = await connection.execute(SELECT_PRODUCT, [name]);
-  return rows[0];
+  try {
+    const [rows] = await connection.execute(SELECT_PRODUCT, [name]);
+    return rows[0];
+  } catch (error) {
+    throw SERVER_ERROR;
+  }
 };
 
 const createProduct = async (name, quantity) => {
   const INSERT_PRODUCT = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)';
-  const [rows] = await connection.execute(INSERT_PRODUCT, [name, quantity]);
-  return rows;
+  try {
+    const [rows] = await connection.execute(INSERT_PRODUCT, [name, quantity]);
+    return rows;
+  } catch (error) {
+    throw SERVER_ERROR;
+  }
 };
 
 module.exports = { searchProductByName, createProduct };
