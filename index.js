102569c9a07dcdfd5ca070721387a3247714104f
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-02 02:48:03 -0300

    Refatoração para verificar testes no gh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const productsRouter = require('./routers/productsRouter');
-require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
