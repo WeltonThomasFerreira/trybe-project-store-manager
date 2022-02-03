1015f704b9d679a5c66428916715957d2d98cb55
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-03 01:28:03 -0300

    Implementa requisito 5

--- services/salesService.js
+++ services/salesService.js
@@ -1,5 +1,4 @@
 const SalesModel = require('../models/salesModel');
-const ProductsService = require('./productsService');
 
 const error = {
   ID_IS_REQUIRED: { code: 400, message: '"product_id" is required' },
@@ -10,20 +9,31 @@ const error = {
   },
 };
 
-const product = async (arg) => ({
-  isUndefined: (data = arg) => !data && data !== 0,
-  isNotNumber: (data = arg) => typeof data !== 'number' || data <= 0,
-  doesNotExist: async (id = arg) => ProductsService.getProductById(id),
-});
+const validateProductId = (products) => {
+  if (products.some((product) => !product[0])) throw error.ID_IS_REQUIRED;
+  return true;
+};
+
+const validateProductQuantity = (products) => {
+  if (products.some((product) => !product[1] && product[1] !== 0)) {
+    throw error.QUANTITY_IS_REQUIRED;
+  }
+  if (
+    products.some(
+      (product) => product[1] <= 0 || typeof product[1] !== 'number',
+    )
+  ) {
+    throw error.QUANTITY_MUST_BE_NUMBER;
+  }
+  return true;
+};
 
 const createSale = async (products) => {
   const productIdKey = 'product_id';
-  console.log(products);
   try {
-    // validateProductId(products);
-    // validateProductQuantity(products);
+    validateProductId(products);
+    validateProductQuantity(products);
     const saleId = await SalesModel.createSale();
-    console.log(saleId);
     const newSale = products.map((item) => [saleId, item[0], item[1]]);
     await SalesModel.createSalesProducts(newSale);
     const sales = await SalesModel.getSalesById(saleId);
@@ -31,13 +41,11 @@ const createSale = async (products) => {
       [productIdKey]: sale.product_id,
       quantity: sale.quantity,
     }));
-    return { code: 200, message: { id: saleId, itemsSold: items } };
+    return { code: 201, message: { id: saleId, itemsSold: items } };
   } catch (e) {
     console.log(e);
     throw e;
   }
 };
-
-// [sales.product_id, sales.quantity]
 
 module.exports = { createSale };
