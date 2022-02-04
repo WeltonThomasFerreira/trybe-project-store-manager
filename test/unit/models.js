2e6011a0ff34778914058b522c568b7a73a8e64b
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-04 01:54:29 -0300

    Teste de salesModel em 74%

--- test/unit/models.js
+++ test/unit/models.js
@@ -3,6 +3,7 @@ const { expect } = require("chai");
 
 const connection = require("../../models/connection");
 const ProductsModel = require("../../models/productsModel");
+const SalesModel = require("../../models/salesModel");
 const { it } = require("mocha");
 
 describe("Testa productsModel", () => {
@@ -145,10 +146,170 @@ describe("Testa productsModel", () => {
         connection.execute.restore();
       });
 
-      it('Retorna undefined', async () => {
+      it("Retorna undefined", async () => {
         const response = await ProductsModel.getProductById(1);
         expect(response).to.be.undefined;
+      });
+    });
+  });
+
+  describe("Atualiza produto por id", () => {
+    describe("Produto atualizado com sucesso", () => {
+      before(async () => {
+        const execute = [{ affectedRows: 1 }];
+        sinon.stub(connection, "execute").resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Retorna propriedade affectRows", async () => {
+        const response = await ProductsModel.updateProductById(1);
+        expect(response).to.be.equal(1);
+      });
+    });
+  });
+
+  describe("Deleta produto por id", () => {
+    describe("Produto deletado com sucesso", () => {
+      before(async () => {
+        const execute = [{ affectedRows: 1 }];
+        sinon.stub(connection, "execute").resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Retorna propriedade affectRows", async () => {
+        const response = await ProductsModel.deleteProductById(1);
+        expect(response)
+          .to.be.an("object")
+          .to.have.property("affectedRows")
+          .equal(1);
+      });
+    });
+  });
+});
+
+describe("Testa salesModel", () => {
+  describe("Insere uma nova venda na tabela sales", () => {
+    describe("Inserção feita com sucesso", () => {
+      before(async () => {
+        const execute = [{ insertId: 1 }];
+        sinon.stub(connection, "execute").resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Retorna insertId", async () => {
+        const response = await SalesModel.createSale();
+        expect(response).to.equal(1);
+      });
+    });
+  });
+
+  describe("Insere uma nova venda na tabela sales_products", () => {
+    describe("Inserção feita com sucesso", () => {
+      before(async () => {
+        const execute = [{ affectedRows: 1 }];
+        sinon.stub(connection, "query").resolves(execute);
+      });
+
+      after(async () => {
+        connection.query.restore();
+      });
+
+      it("Retorna affectedRows", async () => {
+        const SALE = [[1, 2, 2]];
+        const response = await SalesModel.createSalesProducts(SALE);
+        expect(response)
+          .to.be.an("object")
+          .to.have.property("affectedRows")
+          .to.equal(1);
+      });
+    });
+  });
+
+  describe("Lista todas as vendas", () => {
+    describe("Recupera vendas com sucesso", () => {
+      const sale = {
+        saleId: 1,
+        date: "2022-02-04T06:21:49.000Z",
+        product_id: 2,
+        quantity: 2,
+      };
+
+      before(async () => {
+        const execute = [[sale]];
+        sinon.stub(connection, "execute").resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Retorna array de objeto de vendas", async () => {
+        const response = await SalesModel.getAllSales();
+        expect(response).to.be.an("array").to.include(sale);
+      });
+    });
+
+    describe("Não existe vendas pra recuperar", () => {
+      before(async () => {
+        const execute = [];
+        sinon.stub(connection, "execute").resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Retorna undefined", async () => {
+        const response = await SalesModel.getAllSales();
+        expect(response).to.be.undefined;
+      });
+    });
+  });
+
+  describe("Lista vendas por id", () => {
+    describe('Recupera venda com sucesso', () => {
+      const sale = { date: '2022-02-04T07:35:13.000Z', product_id: 1, quantity: 2 };
+      before(async () => {
+        const execute = [[ sale ]]
+        sinon.stub(connection, 'execute').resolves(execute);
       })
+
+      after(async()=>{
+        connection.execute.restore();
+      })
+
+      it('Retorna array com objeto de vendas', async () =>{
+        const response = await SalesModel.getSalesById(1);
+        expect(response).to.be.an('array').to.contains(sale);
+      })
+    })
+  });
+
+  describe("Atualiza venda por id", () => {
+    describe("Venda atualizado com sucesso", () => {
+      before(async () => {
+        const execute = [{ affectedRows: 1 }];
+        sinon.stub(connection, "execute").resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Retorna um objeto com a propriedade affectRows", async () => {
+        const sales = [ { product_id: 1, quantity: 2 } ]
+        const response = await SalesModel.updateSaleById(1, sales);
+        expect(response).to.be.an('object').to.have.property('affectedRows').to.equal(1);
+      });
     });
   });
 });
